9a577e37f84d55f3f9345081627ba6e937a4a0da
Teeandnarita <128231239+Teeandnarita@users.noreply.github.com>
2025-09-28 16:23:51 +0700

    calculator.js

---
+++ calculator.js
@@ -0,0 +1,18 @@
+function cal(a,b,c){
+    
+    if(b == "+"){
+        d = a+c
+    } else if(b == "*"){
+        d = a*c
+    } else if(b == "/"){
+        d = a/c
+    } else{
+        d = a-c
+    }
+    
+    console.log(a, b, c, "=", d )
+}
+
+cal(1,"+",3)
+
+https://www.programiz.com/online-compiler/8s9JvoKENIEkM
